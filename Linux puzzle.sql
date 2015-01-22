7e1502d955a2df20501847e5f8bcda8da5616a0a
bol0bal4 <vunhan93@gmail.com>
2015-01-23 00:13:03 +0700

    Add database - Q/A

---
+++ Linux puzzle.sql
@@ -0,0 +1,42 @@
+--INSERT INTO Challenge VALUES (7, 100, "Chương trình nào của Linux dùng để biên dịch một chương trình được viết bằng ngôn ngữ C","", "ctt{GCC}", 2, 0);
+
+INSERT INTO Challenge VALUES (8, 150, "Câu lệnh nào giúp rename nhanh file/directory trong Linux theo kiểu [command] tên_file_cũ tên_file_mới",
+ "<ul>
+<li>mv</li>
+<li>cp</li>
+<li>rename</li>
+<li>chname</li>
+</ul>", "ctt{mv}", 2, 0);
+
+INSERT INTO Challenge VALUES (9, 100, "Ai được xem là cha đẻ của Linux", "", "ctt{Linus Benedict Torvalds}", 2, 0);
+
+INSERT INTO Challenge VALUES (9, 100, "Đoạn code sau đây mang ý nghĩa in chuỗi 'Hello World', bạn hãy cho biết nó được viết bằng ngôn ngữ nào? ++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", "", "ctt{brainfuck}", 2, 0);
+
+INSERT INTO Challenge VALUES (14, 200, "Nêu hướng giải quyết thông báo lỗi sau đây:", "(img src='http://a.pomf.se/lkaneg.jpg')", "", 2, 1);
+
+INSERT INTO Challenge VALUES (15, 250, "Mình cài song song win 8.1 và ubuntu 14.04. Nhưng mỗi lần bật win là thời gian lại sai, mặc dù đã chỉnh đúng múi giờ... Lỗi xảy ra từ khi mình xài ubuntu... Giờ bên ubuntu thì vẫn đúng bình thường. Có ai biết lỗi gì không? Các bạn hãy chọn 1 link theo bạn có khả năng fix được lỗi này.",""
+, "", 2, 1);
+
+
+INSERT INTO Challenge VALUES (16, 300, "Viết một chương trình tính tổng 2 số nguyên nhập vào từ bàn phím. (Viết bằng C/C++ - Sử dụng GCC/G++ để compile.",
+"Upload src và bin lên host rồi submit link.", "", 2, 1);
+
+
+INSERT INTO Challenge VALUES (17, 350, "Thực hiện lần lượt các công việc sau và submit các commit cần thiết (thực hành trên terminal)",
+"<ul>
+<li>In ra <em>biến môi trường</em> <strong>$PATH</strong></li>
+<li>Tạo thư mục ~/script</li>
+<li>Tạo 1 file CTT.sh với nội dung lấy từ <iframe src='http://pastebin.com/embed_iframe.php?i=zjmpdsj9' style='border:none;width:100%''></iframe></li>
+<li>Set quyền -rwxr-xr-x cho file CTT.sh - Yêu cầu sử dụng hình thức set bằng số (VD: command 777 file)</li>
+<li>Di chuyển vào thư mục ~/script và thực thi file CTT.sh</li>
+<li>Edit biến $PATH sao cho có thể thực thi file CTT.sh từ ~/Desktop.</li>
+<li>In ra <em>biến môi trường</em> <strong>$PATH</strong></li>
+</ul>",
+"", 2, 1);
+
+INSERT INTO Challenge VALUES (18, 200, "Nội dung được trích dẫn sau đây thuộc giấy phép mã nguồn mở nào? (Ghi rõ tên và phiên bản)",
+"All rights granted under this License are granted for the term of copyright on the Program, and are irrevocable provided the stated conditions are met. This License explicitly affirms your unlimited permission to run the unmodified Program. The output from running a covered work is covered by this License only if the output, given its content, constitutes a covered work. This License acknowledges your rights of fair use or other equivalent, as provided by copyright law.<br>You may make, run and propagate covered works that you do not convey, without conditions so long as your license otherwise remains in force. You may convey covered works to others for the sole purpose of having them make modifications exclusively for you, or provide you with facilities for running those works, provided that you comply with the terms of this License in conveying all material for which you do not control copyright. Those thus making or running the covered works for you must do so exclusively on your behalf, under your direction and control, on terms that prohibit them from making any copies of your copyrighted material outside their relationship with you.<br>Conveying under any other circumstances is permitted solely under the conditions stated below. Sublicensing is not allowed; section 10 makes it unnecessary.", "ctt{GPLv3}", 2, 0);
+
+INSERT INTO Challenge VALUES (19, 500, "Viết chương trình hiện thực hóa ngôn ngữ HQ9+ (đưọc chọn ngôn ngữ tùy ý)", "", "", 2, 1);
+
+INSERT INTO Challenge VALUES (20, 200, "Cung cấp id của group Facebook: 'UIT LUG'", "UIT LUG - Linux User Group at UIT", "ctt{662831633832410}", 2, 0);
